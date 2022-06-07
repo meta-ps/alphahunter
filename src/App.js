500087ef55bc0e8caa2940d61042e84a2ac696fa
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-06-07 16:43:00 +0530

    working
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import { Connection } from "@solana/web3.js";
+
 
 function App() {
+
+  const [wallet, setWallet] = useState({});
+  const [walletaddress, setWalletaddress] = useState('');
+  async function connectWallet() {
+
+    try {
+      setWallet(await window.solana.connect());
+      setWalletaddress(window.solana.publicKey.toString());
+      console.log(walletaddress);
+      const connectionMetaplex = new Connection(
+        "https://api.metaplex.solana.com",
+        "confirmed"
+      );
+      const t_walletAddress = walletaddress
+      const nftsmetadata = await Metadata.findDataByOwner(connectionMetaplex, t_walletAddress);
+      console.log(nftsmetadata);
+
+
+    } catch (err) {
+      // { code: 4001, message: 'User rejected the request.' }
+    }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Test</h1 >
+      <button onClick={connectWallet}>Connect to Wallet</button>
+
+    </div >
   );
 }
 
