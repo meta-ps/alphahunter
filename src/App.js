5ac017b6e77d969ec7530a97e5968c049074c12c
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-07-01 11:16:31 +0530

    add: hidden state for button
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { Connection } from "@solana/web3.js";
 function App() {
   const [wallet, setWallet] = useState({});
   const [walletaddress, setWalletaddress] = useState("");
-
+  const [buttonHidden, setButtonHidden] = useState(true);
   const [data, setData] = useState();
 
   const [image, setImage] = useState([]);
   async function connectWallet() {
+    setButtonHidden(false);
+
     try {
       setWallet(await window.solana.connect());
       setWalletaddress(window.solana.publicKey.toString());
@@ -28,7 +30,6 @@ function App() {
       setData(nftsmetadata);
 
       let imagesData = [];
-
       nftsmetadata.map(async (el) => {
         const data = await fetch(el.data.uri);
         const dt = await data.json();
@@ -49,17 +50,23 @@ function App() {
 
   return (
     <div>
-      <h1>Test</h1>
-      <button onClick={connectWallet} className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">Connect to Wallet</button>
-      {image &&
-        image.map((el) => {
-          return (
-            <div key={el.name} >
-              <p>{el.name}</p>
-              <img src={el.image} alt={el.name} width="100px" height="100px" />
-            </div>
-          );
-        })}
+      <div>
+        {buttonHidden && <button onClick={connectWallet} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Connect to Wallet</button>}
+      </div>
+      <div>
+        <h1>Test</h1>
+
+        {image &&
+          image.map((el) => {
+            return (
+              <div key={el.name} >
+                <p>{el.name}</p>
+                <img src={el.image} alt={el.name} width="100px" height="100px" />
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
   );
 }
