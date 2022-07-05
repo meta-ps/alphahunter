d3d12c5835ed4509e3138a5e78649e3ced662c7a
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-07-05 15:53:47 +0530

    fix: typo
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- src/App.js
+++ src/App.js
@@ -58,29 +58,23 @@ function App() {
       <div>
         <h1>Test</h1>
 
-        <div class="container my-12 mx-auto px-4 md:px-12">
-          <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+        <div className="container my-12 mx-auto px-4 md:px-12">
+          <div className="flex flex-wrap -mx-1 lg:-mx-4">
 
-
-
-
-
-            </div>
 
             {image &&
               image.map((el) => {
                 return (
 
-                  <article class="overflow-hidden rounded-lg shadow-lg">
+                  <article className="overflow-hidden rounded-lg shadow-lg">
 
                     <a href="#">
-                      <img alt={el.name} class="block h-auto w-full" src={el.image} />
+                      <img alt={el.name} className="block h-auto w-full" src={el.image} />
                     </a>
 
-                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-                      <h1 class="text-lg">
-                        <a class="no-underline hover:underline text-black" href="#">
+                    <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                      <h1 className="text-lg">
+                        <a className="no-underline hover:underline text-black" href="#">
                           {el.name}
                         </a>
                       </h1>
@@ -88,16 +82,16 @@ function App() {
 
                     </header>
 
-                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                      <a class="flex items-center no-underline hover:underline text-black" href="#">
-                        <img alt="Placeholder" class="block rounded-full" src="https://picsum.photos/32/32/?random" />
-                        <p class="ml-2 text-sm">
+                    <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                      <a className="flex items-center no-underline hover:underline text-black" href="#">
+                        <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
+                        <p className="ml-2 text-sm">
                           {el.name}
                         </p>
                       </a>
-                      <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                        <span class="hidden">Like</span>
-                        <i class="fa fa-heart"></i>
+                      <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                        <span className="hidden">Like</span>
+                        <i className="fa fa-heart"></i>
                       </a>
                     </footer>
 
